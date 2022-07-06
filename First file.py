002e417c9495e53fbb9c499ef2b1efa125c02b53
payel <payel.nandy2580@gmail.com>
2022-07-06 17:01:23 -0400

    Lab 1

---
+++ First file.py
@@ -0,0 +1,29 @@
+x = 5;
+y = 10;
+z = 2.5;
+print(x+y)
+print(y-z)
+print(x *y)
+print(y/x)
+print(y//z)
+print(y/z)
+print(y ** x)
+print(x % y)
+
+
+print("Payel" * 5)
+
+first_name = "Payel"
+last_name = "Banerjee"
+full_name = first_name + " "+ last_name
+print(full_name)
+temp = int (input("Enter temperature in Fahrenheit"))
+celsius = (temp - 32) * 5//9
+print(f"{temp} degrees fahrenheit is equal to {celsius} degree(s) celsius")
+
+
+
+
+
+
+
